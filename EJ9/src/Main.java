9b2b25c17f736275f38d8cc16f452fd6214de4b3
Matias-Louzao-Ataria <louzao.ataria.matias@gmail.com>
2020-10-19 21:50:14 +0200

    EJ9 acabado

--- EJ9/src/Main.java
+++ EJ9/src/Main.java
@@ -3,6 +3,7 @@ import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,82 +12,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        /*String home = System.getProperty("user.home"), sep = System.getProperty("file.separator");
-        File f = new File(home + sep + "alumnos.dat");
-
-        if(!f.exists()){
-            f.createNewFile();
-        }
-
-        FileOutputStream out = new FileOutputStream(f,true);
-        DataOutputStream output = new DataOutputStream(out);
-        output.writeUTF("|");
-        output.writeInt(2);
-        output.writeInt(3);
-        output.writeDouble(4.2);
-        output.close();
-        out.close();
-        FileInputStream in = new FileInputStream(f);
-        DataInputStream input = new DataInputStream(in);
-        System.out.println(input.readUTF());
-        System.out.println(input.readInt());
-        System.out.println(input.readInt());
-        System.out.println(input.readDouble());
-        input.close();
-        in.close();*/
+        /*
+         * String home = System.getProperty("user.home"), sep =
+         * System.getProperty("file.separator"); File f = new File(home + sep +
+         * "alumnos.dat");
+         * 
+         * if(!f.exists()){ f.createNewFile(); }
+         * 
+         * FileOutputStream out = new FileOutputStream(f,true); DataOutputStream output
+         * = new DataOutputStream(out); output.writeUTF("|"); output.writeInt(2);
+         * output.writeInt(3); output.writeDouble(4.2); output.close(); out.close();
+         * FileInputStream in = new FileInputStream(f); DataInputStream input = new
+         * DataInputStream(in); System.out.println(input.readUTF());
+         * System.out.println(input.readInt()); System.out.println(input.readInt());
+         * System.out.println(input.readDouble()); input.close(); in.close();
+         */
 
         Scanner sc = new Scanner(System.in);
-        int elect = 0,id = 0;
         Operaciones o = new Operaciones(sc);
-        while(elect != 5){
-            System.out.println("1.- Dar de alta.\n2.- Consultar alumno.\n3.- Modificar alumno.\n4.- Borar alumno.\n5.- Salir.\n");
-            elect = Operaciones.pedirEntero(sc);
-            switch (elect) {
-                case 1:
-                    o.entradaDeDatos(true, 0);
-                    break;
-
-                case 2:
-                    System.out.println("Introduce la id del alumno a consultar:");
-                    id = Operaciones.pedirEntero(sc);
-                    o.consultarAlumno(id);
-                    break;
-
-                case 3:
-                    System.out.println("Introduce la id del alumno a modificar:");
-                    id = Operaciones.pedirEntero(sc);
-                    o.modificarAlumno(id);
-                    break;
-
-                case 4:
-                    System.out.println("Introduce la id del alumno a eliminar:");
-                    id = Operaciones.pedirEntero(sc);
-                    o.borrarAlumno(id);
-                    break;
-
-                case 5:
-                    System.out.println("Chao!");
-                    break;
-
-                default:
-                    System.out.println("Acción no reconocida!");
-            }
-        }
+        o.menu();
         sc.close();
     }
 }
 
-class Alumno{
+class Alumno {
 
     private String nombre;
     private int id;
     private int fecha;
 
-    public Alumno(){
-        this(0,"a",0);
+    public Alumno() {
+        this(0, "a", 0);
     }
 
-    public Alumno(int fecha,String nombre,int id){
+    public Alumno(int fecha, String nombre, int id) {
         this.setNombre(nombre);
         this.setFecha(fecha);
         this.setId(id);
@@ -97,9 +56,9 @@ class Alumno{
     }
 
     public void setNombre(String nombre) {
-        if(nombre.length() <= 0){
+        if (nombre.length() <= 0) {
             this.nombre = "a";
-        }else{
+        } else {
             this.nombre = nombre;
         }
     }
@@ -122,31 +81,29 @@ class Alumno{
 
 }
 
-
-
-class Operaciones{//TODO Fecha
+class Operaciones {
     private ArrayList<Alumno> alumnos = new ArrayList<Alumno>();
     private String home = System.getProperty("user.home"), sep = System.getProperty("file.separator");
     private File f = new File(home + sep + "alumnos.dat");
     private Scanner sc;
 
-    public Operaciones(Scanner sc){
+    public Operaciones(Scanner sc) {
         this.sc = sc;
         try {
             if (f.exists() && f.length() > 0) {
                 FileInputStream in = new FileInputStream(f);
                 DataInputStream input = new DataInputStream(in);
                 try {
-                    while(true){
-                        Alumno actual = new Alumno(input.readInt(),input.readUTF(),input.readInt());
+                    while (true) {
+                        Alumno actual = new Alumno(input.readInt(), input.readUTF(), input.readInt());
                         alumnos.add(actual);
                     }
                 } catch (EOFException e) {
-                    System.out.println("Fin del archivo!");
+                    //System.out.println("Fin del archivo!");
                 }
                 input.close();
                 in.close();
-            }else{
+            } else {
                 f.createNewFile();
             }
         } catch (IOException e) {
@@ -155,22 +112,65 @@ class Operaciones{//TODO Fecha
         }
     }
 
-    public void consultarAlumno(int id){
+    public void menu(){
+        int id = 0,elect = 0;
+        while (elect != 5) {
+            System.out.println("1.- Dar de alta.\n2.- Consultar alumno.\n3.- Modificar alumno.\n4.- Borar alumno.\n5.- Salir.\n");
+            elect = pedirEntero();    
+            switch (elect) {
+                case 1:
+                    entradaDeDatos();
+                    break;
+
+                case 2:
+                    System.out.println("Introduce la id del alumno a consultar:");
+                    id = pedirEntero();
+                    consultarAlumno(id);
+                    break;
+
+                case 3:
+                    System.out.println("Introduce la id del alumno a modificar:");
+                    id = pedirEntero();
+                    modificarAlumno(id);
+                    break;
+
+                case 4:
+                    System.out.println("Introduce la id del alumno a eliminar:");
+                    id = pedirEntero();
+                    borrarAlumno(id);
+                    break;
+
+                case 5:
+                    System.out.println("Chao!");
+                    break;
+
+                default:
+                    System.out.println("Acción no reconocida!");
+            }
+        }
+        guardar();
+    }
+
+    public void consultarAlumno(int id) {
         boolean echo = false;
         for (Alumno alumno : alumnos) {
-            if(alumno.getId() == id){
+            if (alumno.getId() == id) {
                 String fecha = String.valueOf(alumno.getFecha());
-                if(fecha.length() > 7){
-                System.out.printf("Nombre: %s, Fecha de nacimiento: %s/%s/%s, ID: %d.\n", alumno.getNombre(),fecha.substring(0, 2), fecha.substring(2, 4), fecha.substring(4, fecha.length()),alumno.getId());
+                if (fecha.length() > 7) {
+                    System.out.printf("Nombre: %s, Fecha de nacimiento: %s/%s/%s, ID: %d.\n", alumno.getNombre(),
+                            fecha.substring(0, 2), fecha.substring(2, 4), fecha.substring(4, fecha.length()),
+                            alumno.getId());
 
-                }else{
-                System.out.printf("Nombre: %s, Fecha de nacimiento: %s/%s/%s, ID: %d.\n", alumno.getNombre(),fecha.substring(0, 1), fecha.substring(1, 3), fecha.substring(3, fecha.length()),alumno.getId());
+                } else {
+                    System.out.printf("Nombre: %s, Fecha de nacimiento: %s/%s/%s, ID: %d.\n", alumno.getNombre(),
+                            fecha.substring(0, 1), fecha.substring(1, 3), fecha.substring(3, fecha.length()),
+                            alumno.getId());
 
                 }
                 echo = true;
             }
         }
-        if(!echo){
+        if (!echo) {
             System.out.println("No se encuentra el alumno!");
         }
     }
@@ -185,9 +185,9 @@ class Operaciones{//TODO Fecha
             }
         }
 
-        if(mod){
-            entradaDeDatos(false, pos);
-        }else{
+        if (mod) {
+            entradaDeDatos(pos,id);
+        } else {
             System.out.println("No se encuentra el alumno!");
         }
 
@@ -203,31 +203,52 @@ class Operaciones{//TODO Fecha
         }
         if (!echo) {
             System.out.println("No se encuentra el alumno!");
-        }else{
+        } else {
             System.out.println("Se ha eliminado correctamente el almuno!");
         }
     }
 
-    public void darDeAlta(int fecha,String nombre,int id,boolean echo){
-        try {
-            FileOutputStream out = new FileOutputStream(f,true);
+    public void darDeAlta(int fecha, String nombre, int id, boolean echo) {
+        alumnos.add(new Alumno(fecha, nombre, id));
+        if(echo){
+            System.out.println("Alumno agregado correctamente!");
+        }
+    }
+
+    public void guardar() {
+        f.delete();
+        try(FileOutputStream out = new FileOutputStream(f,true)){ 
             DataOutputStream output = new DataOutputStream(out);
-            output.writeInt(fecha);
-            output.writeUTF(nombre);
-            output.writeInt(id);
-            output.close();
-            out.close();
-            alumnos.add(new Alumno(fecha,nombre,id));
-            if(echo){
-                System.out.println("Alumno agregado correctamente!");
+            for (Alumno alumno : alumnos) {
+                output.writeInt(alumno.getFecha());
+                output.writeUTF(alumno.getNombre());
+                output.writeInt(alumno.getId());
             }
+            output.close();
         } catch (IOException e) {
             System.err.println(e.getLocalizedMessage());
         }
         
     }
 
-    public void entradaDeDatos(boolean alta,int pos){
+    public void entradaDeDatos(int pos,int id){
+        String nombre = "";
+        int fecha = 0;
+        while(fecha == 0){
+            System.out.println("Introduce el nombre del alumno a agregar:");
+            nombre = sc.nextLine();
+            System.out.println("Introduce la fecha de nacimiento del alumno a agregar (ddmmyyyy):");
+            fecha = pedirEntero();
+            if(String.valueOf(fecha).length() > 8 || String.valueOf(fecha).length() < 6){
+                System.out.println("Fecha incorrecta!");
+                fecha = 0;
+            }else{
+                alumnos.set(pos, new Alumno(fecha, nombre, id));
+            }
+        }
+    }
+
+    public void entradaDeDatos(){
         String nombre = "";
         int id = 0;
         int fecha = 0;
@@ -235,20 +256,15 @@ class Operaciones{//TODO Fecha
             System.out.println("Introduce el nombre del alumno a agregar:");
             nombre = sc.nextLine();
             System.out.println("Introduce la fecha de nacimiento del alumno a agregar (ddmmyyyy):");
-            fecha = pedirEntero(sc);
-            System.out.println(String.valueOf(fecha).length());
+            fecha = pedirEntero();
             if(String.valueOf(fecha).length() > 8 || String.valueOf(fecha).length() < 6){
                 System.out.println("Fecha incorrecta!");
                 fecha = 0;
             }else{
                 System.out.println("Introduce la id del alumno a agregar:");
-                id = pedirEntero(sc);
+                id = pedirEntero();
                 if(comprobarId(id)){
-                    if(alta){
-                        darDeAlta(fecha, nombre, id, true);
-                    }else{
-                        alumnos.set(pos,new Alumno(fecha,nombre,id));
-                    }
+                    darDeAlta(fecha, nombre, id, true);
                 }else{
                     System.out.println("ID repetida!");
                     id = 0;
@@ -266,7 +282,7 @@ class Operaciones{//TODO Fecha
         return true;
     }
     
-    public static int pedirEntero(Scanner sc) {
+    public int pedirEntero() {
         int res = 0;
         try {
             res = sc.nextInt();
