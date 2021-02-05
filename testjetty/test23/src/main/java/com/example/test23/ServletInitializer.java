449481b6f113f93ec015bca20f0d0d67c9b6f780
Matias-Louzao-Ataria <louzao.ataria.matias@gmail.com>
2021-02-05 13:49:08 +0100

    Pruebas de clase

---
+++ testjetty/test23/src/main/java/com/example/test23/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.test23;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Test23Application.class);
+	}
+
+}
